0b9aca4cd410359f9a46c82dca44713622478c2e
Ricardo Vega <rvega@dataqu.cl>
2026-01-21 18:07:43 -0300

    Endpoint para recuperar los datos del usuario logueado, datos personales del usuario

--- vCard_api/users/api/views.py
+++ vCard_api/users/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
 
 from users.models import User
@@ -25,3 +27,11 @@ class UserApiViewSet(ModelViewSet):
             request.data['password'] = request.user.password
 
         return super().update(request, *args, **kwargs)
+    
+
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
